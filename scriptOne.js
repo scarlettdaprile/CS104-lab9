37715171001f14e8a28fac547d02acd5c7bc4ceb
scarlettdaprile <skd53@njit.edu>
2024-11-21 10:58:48 -0500

    update

--- scriptOne.js
+++ scriptOne.js
@@ -16,9 +16,42 @@ console.log(spell.trim())
 //Question 5
 let quote = "I solemnly swear that I am up to no good"
 console.log(quote.slice(11,16))
-//Question 6 WRONG
+//Question 6
 let firstName2 = "Ron"
 let lastName2 = "Weasley"
-firstName2.concat(lastName2) 
+let fullName = firstName2.concat(lastName2) 
+console.log(fullName)
 //Question 7
-
+let sentence = "Draco is a good wizard"
+sentence.replace('good', 'bad' )
+//Question 8
+let houses = ['Gryffindor', 'Hufflepuff', 'Ravenclaw']
+houses.push ('Syltherin')
+houses.pop ('Syltherin')
+console.log(houses)
+//Question 9 
+let spells = ['Alohomora', 'Lumos', 'Nox']
+spells.unshift ('Accio')
+spells.shift ('Accio')
+console.log(spells)
+//Question 10
+let professors = ['Dumbledore', 'McGonagall', 'Snape', 'Hagrid']
+professors.slice(1,3)
+console.log(professors)
+//Question 11
+let students = ['Neville', 'Seamus', 'Dean', 'Parvati']
+students.splice(1,2)
+console.log(students)
+//Question 12
+let phrase = ' Mischief Managed '
+phrase.trim()
+console.log(phrase.toLowerCase ())
+firstName.concat(phrase)
+//Question 13
+let wizards = ['Harry', 'Hermione', 'Ron']
+wizards.push('Luna', 'Draco')
+console.log(wizards)
+//Question 14
+ let message = 'Welcome to Hogwarts School of Witchcraft and Wizardry'
+ message.slice('Hogwarts')
+ 'castle'.concat (message)
