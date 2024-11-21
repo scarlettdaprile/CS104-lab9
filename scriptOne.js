d762c0aef64d8435ccb46bcf0f3685f9aafa5f22
scarlettdaprile <skd53@njit.edu>
2024-11-21 10:29:43 -0500

    1-6

---
+++ scriptOne.js
@@ -0,0 +1,24 @@
+//Question 1
+let characters = ['Harry', 'Hermione', 'Ron', 'Draco','Luna'];
+for (let i=0; i<=5; i++);
+console.log(characters);
+//Question 2
+let firstName = "Harry"
+let house = "Gryffindor"
+console.log("Welcome", firstName, "of", house,"!")
+//Question 3
+let characterName = "Hermione"
+console.log(characterName.toUpperCase ())
+console.log(characterName.toLowerCase ())
+//Question 4
+let spell = "Expelliarmus"
+console.log(spell.trim())
+//Question 5
+let quote = "I solemnly swear that I am up to no good"
+console.log(quote.slice(11,16))
+//Question 6 WRONG
+let firstName2 = "Ron"
+let lastName2 = "Weasley"
+firstName2.concat(lastName2) 
+//Question 7
+
